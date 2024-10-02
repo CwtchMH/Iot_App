88f3403b5a35ed5dddccc9e4a589e0c86dff3675
CwtchMH <nmhhieuk2@gmail.com>
2024-10-02 09:51:29 +0700

    Done

--- src/MQTT/index.js
+++ src/MQTT/index.js
@@ -1,7 +1,10 @@
 import mqtt from 'mqtt';
 
 // Use the correct WebSocket URL and port
-const client = mqtt.connect('ws://192.168.1.20:9001/mqtt');
+const client = mqtt.connect('ws://192.168.1.20:9001/mqtt', {
+  username: 'ManhHieu',
+  password: 'b21dccn364'
+});
 
 client.on('connect', () => {
     if (client.connected === true) {
